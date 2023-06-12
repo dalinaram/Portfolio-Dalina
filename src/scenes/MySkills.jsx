8eb095b521cce79ac768690700cea550597d19ff
dalinaram <dalinaram@gmail.com>
2023-06-12 15:43:06 +0200

    first commit

---
+++ src/scenes/MySkills.jsx
@@ -0,0 +1,132 @@
+import LineGradient from "../components/LineGradient";
+import useMediaQuery from "../hooks/useMediaQuery";
+import { motion } from "framer-motion";
+
+const MySkills = () => {
+  const isAboveLarge = useMediaQuery("(min-width: 1060px)");
+  return (
+    <section id="skills" className="pt-10 pb-24">
+      {/* HEADER AND IMAGE SECTION */}
+      <div className="md:flex md:justify-between md:gap-16 mt-32">
+        <motion.div
+          className="md:w-1/3"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+          <p className="font-playfair font-semibold text-4xl mb-5">
+            MY <span className="text-red">SKILLS</span>
+          </p>
+          <LineGradient width="w-1/3" />
+          <p className="mt-10 mb-7">
+            Mes principaux atoûts.
+          </p>
+        </motion.div>
+
+        <div className="mt-16 md:mt-0">
+          {isAboveLarge ? (
+            <div
+              className="relative z-0 ml-20 before:absolute before:-top-10 before:-left-10
+              before:w-full before:h-full before:border-2 before:border-blue before:z-[-1]"
+            >
+              <img
+                alt="skills"
+                className="z-10"
+                src="assets/skills-image.png"
+              />
+            </div>
+          ) : (
+            <img alt="skills" className="z-10" src="assets/skills-image.png" />
+          )}
+        </div>
+      </div>
+
+      {/* SKILLS */}
+      <div className="md:flex md:justify-between mt-16 gap-32">
+        {/* LA CREATIVITE */}
+        <motion.div
+          className="md:w-1/3 mt-10"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, y: 50 },
+            visible: { opacity: 1, y: 0 },
+          }}
+        >
+          <div className="relative h-32">
+            <div className="z-10">
+              <p className="font-playfair font-semibold text-5xl">01</p>
+              <p className="font-playfair font-semibold text-3xl mt-3">
+                La Créativité
+              </p>
+            </div>
+            <div className="w-1/2 md:w-3/4 h-32 bg-blue absolute right-0 top-0 z-[-1]" />
+          </div>
+          <p className="mt-5">
+           Pourvue d'un fort côté créatif et sensible au côté esthétique, le métier de développeuse me permet de développer des solutions originales et stimulantes.
+          </p>
+        </motion.div>
+
+        {/* ECOUTE */}
+        <motion.div
+          className="md:w-1/3 mt-10"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ delay: 0.2, duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, y: 50 },
+            visible: { opacity: 1, y: 0 },
+          }}
+        >
+          <div className="relative h-32">
+            <div className="z-10">
+              <p className="font-playfair font-semibold text-5xl">02</p>
+              <p className="font-playfair font-semibold text-3xl mt-3">
+                L'écoute
+              </p>
+            </div>
+            <div className="w-1/2 md:w-3/4 h-32 bg-red absolute right-0 top-0 z-[-1]" />
+          </div>
+          <p className="mt-5">
+            Soucieuse de répondre avec satisfaction à la clientèle, je possède un excellent sens du relationnel et sait être à l'écoute des besoins, afin d'y répondre avec précision. 
+          </p>
+        </motion.div>
+        {/* LA TENACITE */}
+        <motion.div
+          className="md:w-1/3 mt-10"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ delay: 0.4, duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, y: 50 },
+            visible: { opacity: 1, y: 0 },
+          }}
+        >
+          <div className="relative h-32">
+            <div className="z-10">
+              <p className="font-playfair font-semibold text-5xl">03</p>
+              <p className="font-playfair font-semibold text-3xl mt-3">
+                La Ténacité
+              </p>
+            </div>
+            <div className="w-1/2 md:w-3/4 h-32 bg-yellow absolute right-0 top-0 z-[-1]" />
+          </div>
+          <p className="mt-5">
+            Ma motivation et ma détermination dans tout ce que j'entreprend sont ma force pour ne pas me laisser être découragée ou abandonner et faire preuve de patience afin de trouver des solutions aux difficultés recontrées.
+          </p>
+        </motion.div>
+      </div>
+    </section>
+  );
+};
+
+export default MySkills;
